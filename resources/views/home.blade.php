d14ce86b1fa8f671b0434d68f175adb7cdb90ee0
nasfernane <nasfernane@gmail.com>
2021-03-18 09:32:10 +0100

    fix name user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
 <div class="homeContainer">
     <header class="homeContainer__header">
         <p class="homeContainer__header__userName">
-            {{$user->name}}
         </p>
         <form action="./php/controllers/handler.php?task=logout" method="POST" class="homeContainer__header__form">
             <button  class="homeContainer__header__form--btn">Se déconnecter</button>

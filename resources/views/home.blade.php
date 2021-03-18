2b1e5cb4df2054fd649bbf2ff4100ae57ae85875
nasfernane <nasfernane@gmail.com>
2021-03-18 09:54:48 +0100

    fix name user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,13 @@
 <div class="homeContainer">
     <header class="homeContainer__header">
         <p class="homeContainer__header__userName">
+            @if ($user)
+                {{$user}}
+            @endif
         </p>
         <form action="./php/controllers/handler.php?task=logout" method="POST" class="homeContainer__header__form">
             <button  class="homeContainer__header__form--btn">Se déconnecter</button>
         </form>
-        
     </header>
     <h1>Home</h1>
 </div>

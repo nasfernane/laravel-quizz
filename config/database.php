2fd411dadd779800b43fd3977506e0de40a0a276
nasfernane <nasfernane@gmail.com>
2021-03-22 09:14:24 +0100

    push heroku

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => 'heroku',
+    'default' => env('heroku', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'heroku' => [
             'driver' => 'mysql',
-            // 'url' => getenv('DATABASE_URL'),
-            'host' => getenv('DB_HOST'),
-            // 'port' => env('DB_PORT', '3306'),
-            'database' => getenv('DB_DATABASE'),
-            'username' => getenv('DB_USERNAME', 'forge'),
-            'password' => getenv('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

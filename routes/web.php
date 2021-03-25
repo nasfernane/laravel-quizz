7e7d3602915a0f0235086bdc4a524591c04572af
nasfernane <nasfernane@gmail.com>
2021-03-25 20:15:29 +0100

    test noms sur les routes

--- routes/web.php
+++ routes/web.php
@@ -21,27 +21,29 @@ use App\Http\Controllers\QuizzController;
 // pages protégées
 Route::get('/', function() {
     return view ('pages/home');
-});
+})->name('home');
 
 // pages d'authentification
 Route::get('/login', function () {
     return view('pages/login');
-});
-Route::post('/login', function () {
-    return view('pages/login');
-});
+})->name('login');
+
+// Route::post('/login', function () {
+//     return view('pages/login');
+// });
+
 Route::get('/signup', function () {
     return view('pages/signup');
-});
+})->name('signup');
 
 
 
 // requêtes utilisateurs
-Route::post('/createuser', [UserController::class, 'createUser']);
+Route::post('/createuser', [UserController::class, 'createUser'])->name('createUser');
 
 // requetes authentification
-Route::post('/loguser', [AuthController::class, 'logUser']);
-Route::post('/logout', [AuthController::class, 'logOut']);
+Route::post('/loguser', [AuthController::class, 'logUser'])->name('loginUser');
+Route::post('/logout', [AuthController::class, 'logOut'])->name('logOutUser');
 
 // requêtes lexique
 Route::post('/addword', [LexiconController::class, 'addword']);
